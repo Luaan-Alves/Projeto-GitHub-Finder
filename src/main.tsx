91e68b1022f6a24ea38d46f3cc33e8dcd48ea37c
Luan Alves <luan-alves-dev@hotmail.com>
2024-10-20 14:42:46 -0300

    Atualizações de arquivos para fazer o deploy

--- src/main.tsx
+++ src/main.tsx
@@ -1,31 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
-import Home from './routes/Home.tsx'
-import Repositories from './components/Repositories.tsx'
+import Home from './routes/Home.tsx';
+import Repositories from './components/Repositories.tsx';
+import Error from './components/Error.tsx'; // Componente de erro
+import { Routes, Route } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App/>,
-    children: [
-      {
-        path: "/",
-        element: <Home/>,
-      },
-      {
-        path: 'repos/:login',
-        element: <Repositories />,
-      }
-    ]
-  }
-])
+const root = createRoot(document.getElementById('root')!);
 
-createRoot(document.getElementById('root')!).render(
+root.render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <BrowserRouter basename="/Projeto-GitHub-Finder"> {/* Adicione o basename aqui */}
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="repos/:login" element={<Repositories />} />
+          <Route path="*" element={<Error />} /> {/* Rota para erros */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
