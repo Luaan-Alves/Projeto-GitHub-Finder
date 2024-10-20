524ded5023a8f1ff32a5d424da8b0c52057a419c
Luan Alves <luan-alves-dev@hotmail.com>
2024-10-20 14:49:14 -0300

    rotas atualizadas

--- src/main.tsx
+++ src/main.tsx
@@ -13,12 +13,11 @@ const root = createRoot(document.getElementById('root')!);
 
 root.render(
   <StrictMode>
-    <BrowserRouter basename="/Projeto-GitHub-Finder"> {/* Adicione o basename aqui */}
+    <BrowserRouter basename="/Projeto-GitHub-Finder">
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
           <Route path="repos/:login" element={<Repositories />} />
-          <Route path="*" element={<Error />} /> {/* Rota para erros */}
         </Route>
       </Routes>
     </BrowserRouter>
